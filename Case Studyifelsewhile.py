2efd29c31b710e0e8060bba53bb3a663e996cb7f
YailynH <113278101+YailynH@users.noreply.github.com>
2024-02-13 00:13:33 -0700

    Add files via upload

---
+++ Case Studyifelsewhile.py
@@ -0,0 +1,10 @@
+while True:
+    last_name = input('Enter the student\'s last name: ')
+    if last_name == 'ZZZ':
+        break
+    first_name = input('Enter the student\'s first name: ')
+    gpa = float(input('Enter the student\'s GPA: '))
+    if gpa >= 3.5:
+        print(f'{first_name} {last_name} has made the Dean\'s List.')
+    elif gpa >= 3.25:
+        print(f'{first_name} {last_name} has made the Honor Roll.')
